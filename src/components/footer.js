3b74b1bb2da5b80c95b737fb1a5d320bbf4baf81
Chenmeinian Guo <chenmeinianguo@Chenmeinians-MacBook-Air.local>
2023-09-26 17:02:50 -0400

    refactored project with create-react-app; added components

---
+++ src/components/footer.js
@@ -0,0 +1,74 @@
+
+// import Footer from 'rc-footer';
+import React from "react";
+// import 'rc-footer/assets/index.css'; // import 'rc-footer/asssets/index.less';
+import './footer.css'
+
+const SampleFooter = () => ( 
+  
+  <div  className='footer'
+  onClick
+  autoFocus
+  columns={[
+    {
+      title: 'contact us',
+      items: [
+        {
+          title: 'adress',
+          url: 'https://ceo/',
+          openExternal: true,
+        },
+        {
+          title: 'email',
+          url: 'https://ceo/',
+          openExternal: true,
+        },
+        {
+          title: 'phone',
+          url: 'https://ceo/',
+          openExternal: true,
+        },
+      ],
+    },
+    {
+      icon: (
+        <img
+          src="https://gw.alipayobjects.com/zos/rmsportal/nBVXkrFdWHxbZlmMbsaH.svg"
+          alt="more products"
+        />
+      ),
+      title: 'more info',
+      items: [
+        {
+          icon: (
+            <img
+              src="https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg"
+              alt="yuque"
+            />
+          ),
+          title: 'cse5234',
+          url: 'https://com',
+          description: 'cse5234',
+          openExternal: true,
+        },
+        {
+          icon: (
+            <img
+              src="https://gw.alipayobjects.com/zos/rmsportal/uHocHZfNWZOdsRUonZNr.png"
+              alt="yuque"
+            />
+          ),
+          title: 'cse5234',
+          url: 'https://com',
+          description: 'cse5234',
+          openExternal: true,
+        },
+      ],
+    },
+  ]}
+/>
+
+);
+
+
+export default () => <SampleFooter />;
