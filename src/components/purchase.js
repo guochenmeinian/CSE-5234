181f5829e2bcff4e9a9537a5af6a7ccaf6babb44
Johannes Scutt <johannesscutt@gmail.com>
2023-09-29 17:26:05 -0400

    conflict resolution

--- src/components/purchase.js
+++ src/components/purchase.js
@@ -23,7 +23,7 @@ function Purchase() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/purchase/paymentEntry', { state: { order: order } });
+    navigate('paymentEntry', { state: { order: order } });
   }
 
   const handleQuantityChange = (index, value) => {
