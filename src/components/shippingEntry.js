c9ce6591fce1070d4a0d1cd2ab4fdb0775be040b
Chenmeinian Guo <guochenmeinian@outlook.com>
2023-09-29 18:04:00 -0400

    refactor purchaseEntry

---
+++ src/components/shippingEntry.js
@@ -0,0 +1,15 @@
+import { Button, FormControl, InputLabel, Input, Stack } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+function PaymentEntry() {
+    
+
+    return (
+        <div>
+            shippingEntry
+        </div>
+    )
+}
+
+export default PaymentEntry;
