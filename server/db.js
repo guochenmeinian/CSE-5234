d7262c5bd61e0e3e856f55cf6ecf60bf9dd49754
Johannes Scutt <johannesscutt@gmail.com>
2023-10-22 20:02:10 -0400

    Revert "Revert "Added connection to mysql server""
    This reverts commit b13a08f2e2c35cbe00a74e21fff7e2a60f9dfe82.

---
+++ server/db.js
@@ -0,0 +1,10 @@
+var MySQL = require("sync-mysql");
+
+var connection = new MySQL({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db"
+});
+
+module.exports = connection;
