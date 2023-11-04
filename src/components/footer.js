e2137e375d0758832d3a4d2926642d8f84b2e0f7
Chenmeinian Guo <guochenmeinian@outlook.com>
2023-11-04 02:35:05 -0400

    added amplify; re-deployed on AWS; changed title;

--- src/components/footer.js
+++ src/components/footer.js
@@ -36,7 +36,7 @@ const AppFooter = () => (
           {
             icon: <img src="/other-images/footer-deploy-site-icon.png" alt="Deploy Site" />,
             title: 'Deploy Site',
-            url: 'https://main.d2v8q1c89t29jt.amplifyapp.com',
+            url: 'https://main.d3gz5bx2dbf7cg.amplifyapp.com',
             description: '',
             openExternal: true,
           },
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
+
+// Set the document title
+document.title = "RM Hub";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
