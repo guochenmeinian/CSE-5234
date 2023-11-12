da8644bc0917d183702d8d00b2cd8aebff552ec7
Chenmeinian Guo <guochenmeinian@outlook.com>
2023-11-12 03:54:44 -0500

    align products in purchase/product/cart pages

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,65 +1,61 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { API, graphqlOperation } from "aws-amplify";
-import { Stack, Typography, Box, Button } from '@mui/material';
+import { Box, Typography, Button, Paper } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { getProduct } from "../api/queries";
 import { CartContext } from '../context/cartContext';
 
 function Product() {
   const { addToCart } = useContext(CartContext);
-
   const [item, setItem] = useState({});
   const { id } = useParams();
 
   useEffect(() => {
     async function fetchedProduct() {
       try {
-        const response = await API.graphql(graphqlOperation(getProduct, { id: `${id}`, authMode: "AMAZON_COGNITO_USER_POOLS" }));
-        const fetchedProduct = response?.data?.getProduct || [];
-        setItem(fetchedProduct);
+        const response = await API.graphql(graphqlOperation(getProduct, { id }));
+        setItem(response?.data?.getProduct || {});
       } catch (err) {
-        console.error("Error fetching products:", err);
+        console.error("Error fetching product:", err);
       }
     }
-
     fetchedProduct();
-  }, []);
+  }, [id]);
 
   function handleAddClick() {
     addToCart(item);
   }
 
   return (
-    <Stack
-      spacing={{ xs: 1, sm: 2, md: 4 }}
-      sx={{ p: 2 }}
-      my={5}
-      direction="row"
-      useFlexGap
-      flexWrap="wrap"
-    >
-      <img
-        srcSet={item.image || '/other-images/placeholder-image.png'}
-        src={item.image || '/other-images/placeholder-image.png'}
-        alt={item.title}
-        loading="lazy"
-      />
-      <Box>
-        <Typography gutterBottom variant="h5" component="div">
+    <Paper elevation={3} sx={{ p: 2, display: 'flex', margin: 'auto', maxWidth: 900 }}>
+      <Box sx={{ flex: 1 }}>
+        <img
+          src={item.image || '/other-images/placeholder-image.png'}
+          alt={item.title}
+          style={{ width: '100%', height: 'auto', maxHeight: '500px' }}
+          loading="lazy"
+        />
+      </Box>
+      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', ml: 2 }}>
+        <Typography gutterBottom variant="h4" component="div">
           {item.title}
         </Typography>
-      </Box>
-      <Box>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
+          {item.description}
+        </Typography>
+        <Typography variant="h6" color="text.secondary">
           {`$${item.price}`}
         </Typography>
+        <Box>
+          <Button variant="contained" color="primary" onClick={handleAddClick} sx={{ mr: 1 }}>
+            Add to Cart
+          </Button>
+          <Button variant="outlined" color="secondary">
+            Remove
+          </Button>
+        </Box>
       </Box>
-      <Box>
-        <Button variant="contained" onClick={handleAddClick}>
-          Add to Shopping Cart
-        </Button>
-      </Box>
-    </Stack>
+    </Paper>
   );
 }
 
