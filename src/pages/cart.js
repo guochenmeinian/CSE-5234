dec1e8d9ead84597727bf9d48ad36d50e81ffb94
Chenmeinian Guo <guochenmeinian@outlook.com>
2023-11-15 19:32:46 -0500

    update authentication for product/cart page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -13,62 +13,67 @@ import {
 import DeleteIcon from '@mui/icons-material/Delete';
 import getTotalCost from '../hook/getTotalCost';
 import { CartContext } from '../context/cartContext';
+import { Authenticator, Button as AmplifyButton } from '@aws-amplify/ui-react';
 
 function Cart() {
   const { cartItems, increaseAmount, decreaseAmount, clearCart } = useContext(CartContext);
   const navigate = useNavigate();
 
   return (
-    <Box my={5}>
-      <Typography variant="h3" align="center" gutterBottom sx={{ fontWeight: 'bold' }}>
-        Your Shopping Cart
-      </Typography>
-      
-      {cartItems.length === 0 ? (
-        <Typography variant="h6" align="center">Your cart is empty.</Typography>
-      ) : (
-        <Box>
-          {cartItems.map((item) => (
-            <Card key={item.id} sx={{ display: 'flex', mb: 2, alignItems: 'center' }}>
-              <img
-                src={item.image || '/other-images/placeholder-image.png'}
-                alt={item.title}
-                style={{ width: '100px', height: '100px', objectFit: 'cover' }}
-              />
-              <CardContent>
-                <Typography variant="h6">{item.title}</Typography>
-                <Typography variant="body1">Price: ${item.price}</Typography>
-                <Typography variant="body1">Amount: {item.amount}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button onClick={() => increaseAmount(item.id)}>+</Button>
-                <Button onClick={() => decreaseAmount(item.id, item.amount)}>-</Button>
-                <IconButton onClick={() => decreaseAmount(item.id, 1)}>
-                  <DeleteIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          ))}
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Box my={5}>
+          <Typography variant="h3" align="center" gutterBottom sx={{ fontWeight: 'bold' }}>
+            Your Shopping Cart
+          </Typography>
+          
+          {cartItems.length === 0 ? (
+            <Typography variant="h6" align="center">Your cart is empty.</Typography>
+          ) : (
+            <Box>
+              {cartItems.map((item) => (
+                <Card key={item.id} sx={{ display: 'flex', mb: 2, alignItems: 'center' }}>
+                  <img
+                    src={item.image || '/other-images/placeholder-image.png'}
+                    alt={item.title}
+                    style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                  />
+                  <CardContent>
+                    <Typography variant="h6">{item.title}</Typography>
+                    <Typography variant="body1">Price: ${item.price}</Typography>
+                    <Typography variant="body1">Amount: {item.amount}</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button onClick={() => increaseAmount(item.id)}>+</Button>
+                    <Button onClick={() => decreaseAmount(item.id, item.amount)}>-</Button>
+                    <IconButton onClick={() => decreaseAmount(item.id, 1)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              ))}
 
-          <Box sx={{ mt: 2 }}>
-            <Typography variant="h5">Total: ${getTotalCost(cartItems)}</Typography>
-            <Button
-              variant="contained"
-              sx={{ mt: 2 }}
-              onClick={() => navigate('/purchase/payment', { state: { order: cartItems } })}>
-              Proceed to Payment
-            </Button>
-            <Button
-              variant="outlined"
-              color="error"
-              sx={{ ml: 2, mt: 2 }}
-              onClick={clearCart}>
-              Clear Cart
-            </Button>
-          </Box>
+              <Box sx={{ mt: 2 }}>
+                <Typography variant="h5">Total: ${getTotalCost(cartItems)}</Typography>
+                <Button
+                  variant="contained"
+                  sx={{ mt: 2 }}
+                  onClick={() => navigate('/purchase/payment', { state: { order: cartItems } })}>
+                  Proceed to Payment
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="error"
+                  sx={{ ml: 2, mt: 2 }}
+                  onClick={clearCart}>
+                  Clear Cart
+                </Button>
+              </Box>
+            </Box>
+          )}
         </Box>
       )}
-    </Box>
+    </Authenticator>
   );
 }
 
--- src/pages/product.js
+++ src/pages/product.js
@@ -4,6 +4,7 @@ import { Box, Typography, Button, Paper } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { getProduct } from "../api/queries";
 import { CartContext } from '../context/cartContext';
+import { Authenticator, Button as AmplifyButton } from '@aws-amplify/ui-react';
 
 function Product() {
   const { addToCart } = useContext(CartContext);
@@ -27,35 +28,39 @@ function Product() {
   }
 
   return (
-    <Paper elevation={3} sx={{ p: 2, display: 'flex', margin: 'auto', maxWidth: 900 }}>
-      <Box sx={{ flex: 1 }}>
-        <img
-          src={item.image || '/other-images/placeholder-image.png'}
-          alt={item.title}
-          style={{ width: '100%', height: 'auto', maxHeight: '500px' }}
-          loading="lazy"
-        />
-      </Box>
-      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', ml: 2 }}>
-        <Typography gutterBottom variant="h4" component="div">
-          {item.title}
-        </Typography>
-        <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
-          {item.description}
-        </Typography>
-        <Typography variant="h6" color="text.secondary">
-          {`$${item.price}`}
-        </Typography>
-        <Box>
-          <Button variant="contained" color="primary" onClick={handleAddClick} sx={{ mr: 1 }}>
-            Add to Cart
-          </Button>
-          <Button variant="outlined" color="secondary">
-            Remove
-          </Button>
-        </Box>
-      </Box>
-    </Paper>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Paper elevation={3} sx={{ p: 2, display: 'flex', margin: 'auto', maxWidth: 900 }}>
+          <Box sx={{ flex: 1 }}>
+            <img
+              src={item.image || '/other-images/placeholder-image.png'}
+              alt={item.title}
+              style={{ width: '100%', height: 'auto', maxHeight: '500px' }}
+              loading="lazy"
+            />
+          </Box>
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'space-between', ml: 2 }}>
+            <Typography gutterBottom variant="h4" component="div">
+              {item.title}
+            </Typography>
+            <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
+              {item.description}
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              {`$${item.price}`}
+            </Typography>
+            <Box>
+              <Button variant="contained" color="primary" onClick={handleAddClick} sx={{ mr: 1 }}>
+                Add to Cart
+              </Button>
+              <Button variant="outlined" color="secondary">
+                Remove
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
+      )}
+    </Authenticator>
   );
 }
 
