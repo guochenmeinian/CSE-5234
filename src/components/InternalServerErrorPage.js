8db500e440a65efe2fe510e2c6dafa6284bae230
Jachian <author.tle@gmail.com>
2023-12-02 22:03:51 -0500

    add image on error page

--- src/components/InternalServerErrorPage.js
+++ src/components/InternalServerErrorPage.js
@@ -5,16 +5,25 @@ const InternalServerErrorPage = () => {
         <Box>
             <Typography
                 variant="h3"
-                align="center"
                 gutterBottom
                 sx={{ fontWeight: 'bold', color: (theme) => theme.palette.primary.main }}>
                 Whoa, Morty! We're in a bit of a pickle here!
             </Typography>
-            Uh-oh! Looks like we've hit a snag, Morty!
-            Something's gone all wibbly-wobbly on the website!
-            We're working on fixing it faster than you can say 'Rickety Rickety Wrecked!'
-            Hang tight while we dive into the intergalactic troubleshooting.
-            We'll have this glitch sorted out before you can burp like Rick! Stay tuned! 🛠️🌌🌀
+            <Typography
+                variant="h6"
+                gutterBottom>
+                Uh-oh! Looks like we've hit a snag, Morty!
+                Something's gone all wibbly-wobbly on the website!
+                We're working on fixing it faster than you can say 'Rickety Rickety Wrecked!'
+                Hang tight while we dive into the intergalactic troubleshooting.
+                We'll have this glitch sorted out before you can burp like Rick! Stay tuned! 🛠️🌌🌀
+            </Typography>
+            <img
+                srcSet="/other-images/internal-server-error-image.png?fit=crop&auto=format&dpr=2 2x"
+                src="/other-images/internal-server-error-image.png?fit=crop&auto=format"
+                alt=""
+                loading="lazy"
+            />
         </Box>
     )
 };
